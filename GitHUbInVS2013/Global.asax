1b8b6a11ebc8a8a416fe536fe2c839ba001acbba
Yudu <cloudazur.net@outlook.com>
2015-06-25 12:03:44 +1000

    GitTest

---
+++ GitHUbInVS2013/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitHUbInVS2013.MvcApplication" Language="C#" %>
---
+++ GitHUbInVS2013/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHUbInVS2013.Startup))]
+namespace GitHUbInVS2013
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
